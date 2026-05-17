d695c06a97af5da623bd5bb01633eaefb58cbdba
sasidu-dilshan <sasidu11@gmail.com>
2026-05-17 20:13:56 +0530

    removing unneccesory codes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express, { application } from 'express'
 import mongoose from 'mongoose'
-import studentRouter from './routes/studentRouter.js'
 import userRouter from './routes/userRouter.js'
 import authenticateUser from './middlewares/authenticate.js'
 import productRouter from './routes/productRouter.js'
@@ -20,7 +19,6 @@ app.use( express.json() )
 
 app.use(authenticateUser)
 
-app.use("/students", studentRouter)
 app.use("/users", userRouter)
 app.use("/products", productRouter)
  
