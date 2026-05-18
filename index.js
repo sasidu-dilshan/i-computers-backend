f682d36904677a40456c41e70d74a49515bce568
sasidu-dilshan <sasidu11@gmail.com>
2026-05-18 21:38:30 +0530

    dns change

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose'
 import userRouter from './routes/userRouter.js'
 import authenticateUser from './middlewares/authenticate.js'
 import productRouter from './routes/productRouter.js'
+import dns from "node:dns";
 
-const mongoUri = "mongodb+srv://admin:1234@cluster0.cfkk43d.mongodb.net/?appName=Cluster0"
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const mongoUri = "mongodb://admin:1234@ac-441zki5-shard-00-00.g2t2i3c.mongodb.net:27017,ac-441zki5-shard-00-01.g2t2i3c.mongodb.net:27017,ac-441zki5-shard-00-02.g2t2i3c.mongodb.net:27017/?ssl=true&replicaSet=atlas-n8k8yd-shard-0&authSource=admin&appName=Cluster0"
 
 mongoose.connect(mongoUri).then(
     ()=>{
